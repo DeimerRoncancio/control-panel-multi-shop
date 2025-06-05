4ec77d90112ea6b66daa9ae250a557e17728cd93
Emerson Roncancio <roncancioavila@gmail.com>
2025-06-05 17:22:22 -0500

    configuracion de eslint + airbnb + prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,45 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js, cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  ignorePatterns: ['dist', '.eslintrc'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.app.json',
+    ecmaversion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    // 'react-refresh/only-export-components': [
+    //   'warn',
+    //   {
+    //     allowConstantExport: true,
+    //   },
+    // ],
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'crlf',
+      },
+    ],
+    'react/react-in-jsx-scope': 'off',
+  },
+};
